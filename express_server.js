f66c0b68e120cbe4a9c3e0260b7ea024f09cf5d5
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-22 07:20:22 -0800

    edited

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "b2xVn2" };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
