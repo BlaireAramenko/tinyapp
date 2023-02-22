3dee00bef732ab63cef509118301fcb81fae7ada
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-22 12:30:13 -0800

    added redirect

--- express_server.js
+++ express_server.js
@@ -48,8 +48,14 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
